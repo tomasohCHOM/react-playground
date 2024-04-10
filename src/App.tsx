6b816b1f7a77cb153a007fab87a31799f6fa7a5b
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-04-10 14:52:18 -0700

    Add lorem text and move to background.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,43 @@
-import { useEffect, useState } from "react";
+import Background from "./Background";
 import "./globals.css";
-import { motion } from "framer-motion";
-
-const numberOfDots = 70;
-
-function createPositions(maxWidth: number) {
-  return Array.from({ length: numberOfDots }, () => [
-    Math.floor(Math.random() * maxWidth),
-    Math.floor(Math.random() * 400) + 200,
-  ]);
-}
-
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
-}
 
 function App() {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
-
-  const positions = createPositions(windowDimensions.width);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowDimensions(getWindowDimensions());
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
-    <motion.div className="fixed flex w-full h-screen items-center justify-center">
-      <motion.svg className="w-full h-full">
-        {positions.map((pos, i) => (
-          <motion.circle
-            key={`circle${i}`}
-            cx={pos[0]}
-            cy={pos[1]}
-            r="2"
-            fill="#ffffff"
-            stroke="#ffffff"
-            className="opacity-50"
-          />
-        ))}
-      </motion.svg>
-    </motion.div>
+    <>
+      <Background />
+      <p className="max-w-[50ch] mx-auto my-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit aut
+        nemo maiores odio veniam perspiciatis, explicabo architecto minima
+        doloremque, iure eligendi quo perferendis at commodi consectetur totam
+        ex voluptatem deleniti quas animi ab? Praesentium deserunt, nesciunt,
+        maiores cupiditate iste suscipit, assumenda nostrum illum ratione
+        pariatur dolor doloribus nemo neque at!
+      </p>
+      <p className="max-w-[50ch] mx-auto my-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit aut
+        nemo maiores odio veniam perspiciatis, explicabo architecto minima
+        doloremque, iure eligendi quo perferendis at commodi consectetur totam
+        ex voluptatem deleniti quas animi ab? Praesentium deserunt, nesciunt,
+        maiores cupiditate iste suscipit, assumenda nostrum illum ratione
+        pariatur dolor doloribus nemo neque at!
+      </p>
+      <p className="max-w-[50ch] mx-auto my-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit aut
+        nemo maiores odio veniam perspiciatis, explicabo architecto minima
+        doloremque, iure eligendi quo perferendis at commodi consectetur totam
+        ex voluptatem deleniti quas animi ab? Praesentium deserunt, nesciunt,
+        maiores cupiditate iste suscipit, assumenda nostrum illum ratione
+        pariatur dolor doloribus nemo neque at!
+      </p>
+      <p className="max-w-[50ch] mx-auto my-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit aut
+        nemo maiores odio veniam perspiciatis, explicabo architecto minima
+        doloremque, iure eligendi quo perferendis at commodi consectetur totam
+        ex voluptatem deleniti quas animi ab? Praesentium deserunt, nesciunt,
+        maiores cupiditate iste suscipit, assumenda nostrum illum ratione
+        pariatur dolor doloribus nemo neque at!
+      </p>
+    </>
   );
 }
 
