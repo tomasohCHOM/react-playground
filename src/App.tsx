9ea2a2dfd4c5365cfaa745bac0fb07cf2612be2d
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-12-31 13:19:55 -0800

    create constellations grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,72 @@
-import Starfield from "./Starfield";
 import "./globals.css";
+import { motion } from "framer-motion";
+
+interface Coordinates {
+  x: number;
+  y: number;
+}
+
+interface Edge {
+  start: Coordinates;
+  end: Coordinates;
+}
+
+function LineConnector({
+  start,
+  end,
+}: {
+  start: Coordinates;
+  end: Coordinates;
+}) {
+  const path = `M${start.x},${start.y} L ${end.x}, ${end.y}}`;
+  return (
+    <motion.svg>
+      <motion.path
+        d={path}
+        stroke="white"
+        strokeWidth={2}
+        initial={{ pathLength: 0 }}
+        animate={{ pathLength: 1 }}
+        transition={{ duration: 1 }}
+      />
+    </motion.svg>
+  );
+}
+
+function Constellation() {
+  const circlePositions: Coordinates[] = [
+    { x: 650, y: 270 },
+    { x: 750, y: 550 },
+    { x: 250, y: 550 },
+    { x: 400, y: 100 },
+  ];
+  const edges: Edge[] = [
+    { start: { x: 650, y: 270 }, end: { x: 750, y: 550 } },
+    { start: { x: 750, y: 550 }, end: { x: 250, y: 550 } },
+    { start: { x: 400, y: 100 }, end: { x: 250, y: 550 } },
+    { start: { x: 400, y: 100 }, end: { x: 650, y: 270 } },
+  ];
+
+  return (
+    <motion.svg width={1024} height={720}>
+      {circlePositions.map(({ x, y }, i) => (
+        <circle
+          key={"Circle " + i}
+          cx={x}
+          cy={y}
+          r={35}
+          className="fill-white"
+        />
+      ))}
+      {edges.map(({ start, end }, i) => {
+        return <LineConnector key={"Line " + i} start={start} end={end} />;
+      })}
+    </motion.svg>
+  );
+}
 
 function App() {
-  return (
-    <>
-      <Starfield />
-      <h1 className="relative text-7xl font-bold">
-        Tomas <span className="text-[rgb(34,193,153)]">Oh</span>
-        <img className="absolute w-28 top-0 -right-1/3" src="/sephiroth.gif" />
-      </h1>
-    </>
-  );
+  return <Constellation />;
 }
 
 export default App;
